33991fbdab244bfde003eeef3c5c1299d9e409f5
Aniketshinde26 <anikets2633@gmail.com>
2026-08-22 21:26:49 +0530

    github url change

--- frontend/src/services/githubAuth.ts
+++ frontend/src/services/githubAuth.ts
@@ -1,8 +1,11 @@
 const GITHUB_CLIENT_ID = import.meta.env.VITE_GITHUB_CLIENT_ID;
-const REDIRECT_URI = "http://localhost:5173/auth/github/callback";
 
 export const initiateGithubLogin = () => {
-  const githubUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user:email`;
+  const redirectUri = `${window.location.origin}/auth/github/callback`;
+
+  const githubUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent(
+    redirectUri,
+  )}&scope=user:email`;
 
   window.location.href = githubUrl;
 };
